59dc99ecf3344bffc96d793c94b8845a2bf557f3
chris <chris@liquidmetalgames.com>
2013-01-28 16:01:13 -0600

    libpthread-stubs support
    Signed-off-by: chris <chris@liquidmetalgames.com>

--- Android.mk
+++ Android.mk
@@ -8,6 +8,10 @@ GLOBAL_X11_INCLUDES := $(LOCAL_PATH)/include
 #LOCAL_PATH := $(X11_GLOBAL_PATH)
 
 include $(LOCAL_PATH)/libXau-1.0.7/Android.mk
+LOCAL_PATH := $(X11_GLOBAL_PATH)
+
+include $(LOCAL_PATH)/libpthread-stubs-0.3/Android.mk
+LOCAL_PATH := $(X11_GLOBAL_PATH)
 
 LOCAL_PATH := $(X11_GLOBAL_PATH)
 
